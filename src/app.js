2943112c7f7b65a15a33ba22d16c63202f49df03
palachini <mateuspalachini@gmail.com>
2020-01-30 20:48:44 -0400

    chore: Configuring structures

--- src/app.js
+++ src/app.js
@@ -1,15 +1,20 @@
 const express = require('express');
+const routes = require('./routes');
 
 class App {
   constructor(){
     this.server = express();
+
+    this.middlewares();
+    this.routes();
   }
 
   middlewares(){
-    this.server.unsubscribe(express.json());
+    this.server.use(express.json());
   }
 
   routes() {
-
+    this.server.use(routes);
   }
 }
+module.exports = new App().server;
